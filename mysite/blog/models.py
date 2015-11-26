2e7a8aa702e2be88e54cf88ac4e65f7b016fa1cd
Wei Song <ukison@cisco.com>
2015-11-26 15:44:27 +0800

    Added two model -- Book and Author, and created/saved several model instances.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,3 +5,12 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=20)
     timestamp = models.DateTimeField()
     body = models.TextField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(Author)
